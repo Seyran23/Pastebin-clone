47ffc65bd88e5eccee73a9fa37b5e4dc2aa22b76
Seyran Musayev <seyranm230@gmail.com>
2026-05-14 01:27:31 +0200

    feat(client): add global styles, layout and public assets

---
+++ client/app/layout.tsx
@@ -0,0 +1,38 @@
+// src/app/layout.tsx
+import Header from "@/components/Header";
+import "./globals.css";
+import type { Metadata } from "next";
+import Footer from "@/components/Footer";
+import LastestPastes from "@/components/LatestPastes";
+import QueryProvider from "@/components/QueryProvider";
+
+export const metadata: Metadata = {
+  title: "Pastebin Clone",
+  description: "Fast and simple pasting tool",
+};
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-neutral-700 text-neutral-900">
+        <QueryProvider>
+
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <div className="container max-w-[1340px] mx-auto pb-8 grid grid-cols-1 md:grid-cols-[3fr_1fr] rounded-b-xl ">
+              <div className="bg-neutral-800 shadow-sm p-6 ">{children}</div>
+
+              <LastestPastes />
+            </div>
+            <Footer />
+          </div>
+        </QueryProvider>
+      </body>
+    </html>
+  );
+}
